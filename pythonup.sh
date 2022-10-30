7f00fb9211e58f818d2c22702e11559239b199ff
Song921012 <aidishage@gmail.com>
2022-10-29 20:39:07 -0400

    update

--- pythonup.sh
+++ pythonup.sh
@@ -1,14 +1,12 @@
 conda deactivate
 conda activate base
+pip install --upgrade pip
 pip install -U radian conan flake8 pylint yapf\
   SAlib deepxde streamlit plotly-express\
-  tensorflow-probability gplearn gpflow\
-  gpytorch botorch pytorch-forecasting neuralprophet torchdyn torchode torchgan torchsde torchcde torchdiffeq torchmetrics pytorch-lightning  pyro-ppl\
+  tensorflow tensorflow-probability gplearn gpflow\
+  gpytorch botorch pytorch-forecasting neuralprophet torchdyn torchgan torchsde torchcde torchdiffeq torchmetrics pytorch-lightning  pyro-ppl\
   Casadi sbi pints elfi POT lifelines Pulp pysindy pykoopman pysr pmlb HROCH pyoperon pydmd cvxpy probnum POT pylops pyproximal abcpy pyabc\
-  pyomo gekko dymos leafmap darts pydeck nlopt optax flax numpyro jaxns funsor ott distrax efax diffrax jaxopt sgmcmcjax gpjax blackjax pyecharts plotly dash sklearn
-pip install -U "sktime[all_extras]"
-pip install --upgrade pip
+  pyomo gekko dymos leafmap darts pydeck nlopt optax flax numpyro jaxns funsor ott distrax efax diffrax jaxopt sgmcmcjax gpjax blackjax pyecharts plotly dash sklearn "sktime[all_extras]"
 pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-pip install -U tensorflow
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip install -U torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 
