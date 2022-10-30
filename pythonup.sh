bcda1f2f057a5f0a09518774a8329303b1b37eb4
Song921012 <aidishage@gmail.com>
2022-10-30 13:55:21 -0400

    update

--- pythonup.sh
+++ pythonup.sh
@@ -5,7 +5,7 @@ pip install -U radian conan flake8 pylint yapf\
   SAlib deepxde streamlit plotly-express\
   tensorflow tensorflow-probability gplearn gpflow\
   gpytorch botorch pytorch-forecasting neuralprophet torchdyn torchgan torchsde torchcde torchdiffeq torchmetrics pytorch-lightning  pyro-ppl\
-  Casadi sbi pints elfi POT lifelines Pulp pysindy pykoopman pysr pmlb HROCH pyoperon pydmd cvxpy probnum POT pylops pyproximal abcpy pyabc\
+  Casadi sbi pints elfi POT lifelines Pulp pysindy pykoopman pykoop mosek cvxopt smcp pysr pmlb HROCH pyoperon pydmd cvxpy probnum POT pylops pyproximal abcpy pyabc\
   pyomo gekko dymos leafmap darts pydeck nlopt optax flax numpyro jaxns funsor ott distrax efax diffrax jaxopt sgmcmcjax gpjax blackjax pyecharts plotly dash sklearn "sktime[all_extras]"
 pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -U torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
