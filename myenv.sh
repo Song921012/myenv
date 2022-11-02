dc26b891c780509798042b88fcbff6de1a50c752
Song921012 <aidishage@gmail.com>
2022-11-02 17:56:13 -0400

    update

--- myenv.sh
+++ myenv.sh
@@ -24,6 +24,7 @@ sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
 
+# stacer
 sudo add-apt-repository ppa:oguzhaninan/stacer -y
 sudo apt-get update
 sudo apt-get install stacer -y
@@ -51,9 +52,11 @@ sudo apt install system76-cuda-latest
 sudo apt install system76-cudnn-11.2
 
 # Vscode
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code
