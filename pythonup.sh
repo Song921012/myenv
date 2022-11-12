04bb9e9c0ef8151e3348de46c68b1815baff945a
Song921012 <aidishage@gmail.com>
2022-11-12 13:59:12 -0500

    update

--- pythonup.sh
+++ pythonup.sh
@@ -26,11 +26,14 @@ pip install -U radian conan flake8 pylint yapf huggingface-hub\
   tensorflow tensorflow-probability gplearn gpflow\
   gpytorch botorch pytorch-forecasting neuralprophet torchdyn torchgan torchsde torchcde torchdiffeq torchmetrics pytorch-lightning  pyro-ppl\
   Casadi sbi pints elfi POT lifelines Pulp pysindy pykoopman pykoop mosek cvxopt smcp pysr pmlb HROCH pyoperon pydmd cvxpy probnum POT pylops pyproximal abcpy pyabc\
-  pyomo gekko dymos leafmap darts pydeck nlopt optax flax numpyro jaxns funsor ott distrax neural-tangents efax pyMetaheuristic diffrax jaxopt evosax sgmcmcjax gpjax blackjax pyecharts plotly dash "sktime[all_extras]" nodepy seaborn keplergl osmnx pyecharts_snapshot biopython
+  pyomo gekko dymos leafmap darts pydeck nlopt optax flax numpyro jaxns funsor ott distrax neural-tangents efax pyMetaheuristic diffrax jaxopt evosax sgmcmcjax gpjax blackjax pyecharts plotly dash "sktime[all_extras]" nodepy seaborn keplergl osmnx pyecharts_snapshot biopython mesa
 pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -U torch --extra-index-url https://download.pytorch.org/whl/cu116
 pip install -U torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 pip install -U git+https://github.com/probml/dynamax.git
+
+# install abm framework from [FLAMEGPU/FLAMEGPU2: FLAME GPU 2 is a GPU accelerated agent based modelling framework for C++ and Python](https://github.com/FLAMEGPU/FLAMEGPU2)
+
 
 
 # PyMC python
