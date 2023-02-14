93e7f81bd1c70116ef5899286221dccaf8c8ff47
Song921012 <aidishage@gmail.com>
2023-02-14 08:31:43 -0500

    update

--- pythonup.sh
+++ pythonup.sh
@@ -28,8 +28,9 @@ pip install -U radian conan flake8 pylint yapf huggingface-hub\
   Casadi sbi pints elfi POT lifelines Pulp pysindy pykoopman pykoop mosek cvxopt smcp pysr pmlb HROCH pyoperon pydmd cvxpy probnum POT pylops pyproximal abcpy pyabc\
   pyomo gekko dymos leafmap darts pydeck nlopt optax flax numpyro jaxns\
   funsor ott distrax neural-tangents efax dm-haiku pyMetaheuristic diffrax jaxopt evosax sgmcmcjax gpjax blackjax\
-  pyecharts plotly dash "sktime[all_extras]" nodepy seaborn keplergl osmnx pyecharts_snapshot\
+  pyecharts plotly dash "sktime[all_extras]" nodepy seaborn keplergl\
   biopython mesa quadpy scalene emcee casadi-horizon bayesnewton objax
+#osmnx pyecharts_snapshot
 pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -U torch --extra-index-url https://download.pytorch.org/whl/cu116
 pip install -U torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
