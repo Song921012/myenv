c1a3b38872f5f63962cfc3d823136370dbf0e219
Song921012 <aidishage@gmail.com>
2023-02-15 08:08:42 -0500

    update

--- setup_repo.jl
+++ setup_repo.jl
@@ -3,14 +3,14 @@ using PkgTemplates
 tem = Template(;
     user = "Song921012",
     authors = "Pengfei Song",
-    julia = v"1.6",
+    julia = v"1.8",
     plugins = [
         License(; name = "MIT"),
         GitHubActions(),
-        TravisCI(),
-        Codecov(),
-        Coveralls(),
-        AppVeyor(),
+        #TravisCI(),
+        #Codecov(),
+        #Coveralls(),
+       # AppVeyor(),
         Documenter{GitHubActions}(),
     ]
 )
