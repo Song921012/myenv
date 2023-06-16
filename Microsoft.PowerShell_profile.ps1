fd60ce57a38fa4cc0bce02f9e7b80865b13ed35e
Song921012 <aidishage@gmail.com>
2023-06-16 09:42:24 +0800

    update

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,90 @@
+#Invoke-Expression (&starship init powershell)
+
+oh-my-posh init pwsh --config 'D:/Documents/ohmyposhthemes/M365Princess.omp.json' | Invoke-Expression
+
+#M365Princess
+#powerlevel10k_rainbow
+#craver
+
+Import-Module PSConsoleTheme
+
+Set-PSReadLineOption -PredictionSource History # 设置预测文本来源为历史记录
+Set-PSReadlineKeyHandler -Key Tab -Function Complete # 设置 Tab 键补全
+Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function MenuComplete # 设置 Ctrl+d 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo # 设置 Ctrl+z 为撤销
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
+
+function ssh_zhen {
+ssh hpc.abm-lab.yorku.ca -l zhenw
+}
+
+function ssh_nancy {
+ssh hpc.abm-lab.yorku.ca -l nancys
+}
+
+Set-Alias sshzhen ssh_zhen
+Set-Alias sshnancy  ssh_nancy
+
+
+
+#VPN
+
+function set_proxy_variable {
+	$proxy = 'http://127.0.0.1:7890'
+
+    # temporary
+    $env:HTTP_PROXY = $proxy
+    $env:HTTPS_PROXY = $proxy
+
+    # forever
+    # [System.Environment]::SetEnvironmentVariable("HTTP_PROXY", $proxy, "User")
+    # [System.Environment]::SetEnvironmentVariable("HTTPS_PROXY", $proxy, "User")
+    
+    Write-Host "`n   OPEN powershell proxy channel!`n"
+}
+
+function unset_proxy_variable {
+    # temporary
+    Remove-Item env:HTTP_PROXY
+    Remove-Item env:HTTPS_PROXY
+
+    # forever
+    # [Environment]::SetEnvironmentVariable('http_proxy', $null, 'User')
+    # [Environment]::SetEnvironmentVariable('https_proxy', $null, 'User')
+
+    Write-Host "`n   CLOSE powershell proxy channel!`n"
+}
+
+New-Alias vpnon set_proxy_variable
+New-Alias vpnoff unset_proxy_variable
+
+
+# git
+
+function git_sync{
+git pull
+git add -A
+git commit -m 'update'
+git push
+}
+
+New-Alias gitsync git_sync
+
+# juliaup
+function julia_up{
+julia -e 'using Pkg;using Dates;Pkg.update();Pkg.gc(collect_delay = Day(0))'
+}
+
+New-Alias juliaup julia_up
+
+# pythonup
+function python_up{
+powershell.exe -noexit "& 'D:\aidishage\myenv\pythonup.ps1'"
+}
+
+New-Alias pythonup python_up
+
+
+
+
---
+++ pythonup.ps1
@@ -0,0 +1,41 @@
+# Linux python
+conda deactivate
+conda deactivate
+pip install  Numpy Pandas Matplotlib scipy scikit-learn sympy statsmodels networkx seaborn jupyter jupyterlab radian conan flake8 pylint  yapf virtualenv 
+
+# Base python
+conda activate base
+python -m pip install --upgrade pip
+
+pip install Numpy Pandas Matplotlib scipy scikit-learn sympy statsmodels networkx  virtualenv virtualenvwrapper-win rich pygments talk2stat isort
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple echarts-countries-pypkg
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple echarts-china-provinces-pypkg
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple echarts-china-cities-pypkg
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple echarts-china-counties-pypkg
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple echarts-china-misc-pypkg
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple echarts-united-kingdom-pypkg
+  
+# 
+pip install -U radian conan flake8 pylint yapf huggingface-hub SAlib deepxde streamlit plotly-express tensorflow tensorflow-probability gplearn gpflow pyecharts plotly dash nodepy seaborn
+
+## inference and optimization
+
+pip install -U Casadi pints elfi POT lifelines Pulp pysindy pykoopman pykoop mosek cvxopt smcp pysr pmlb HROCH pydmd cvxpy probnum POT pyomo gekko dymos
+
+## torch
+pip install -U gpytorch botorch  torchdyn torchgan torchsde torchcde torchdiffeq torchmetrics torchopt pytorch-lightning  pyro-ppl sbi storchastic
+
+pip install osmnx pyecharts_snapshot
+
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
+
+
+conda activate base
+
+
