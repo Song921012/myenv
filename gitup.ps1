531091bd189cbb09dc070932b6e2f9988926f015
Song921012 <aidishage@gmail.com>
2023-08-22 11:59:24 +0800

    update

--- gitup.ps1
+++ gitup.ps1
@@ -76,6 +76,16 @@ git pull
 git add -A
 git commit -m "update"
 git push
+
+echo "Zotero"
+
+cd C:\Users\12475\AppData\Roaming\Zotero
+
+git pull
+git add -A
+git commit -m "update"
+git push
+
 echo "Done"
 cd ~
 
